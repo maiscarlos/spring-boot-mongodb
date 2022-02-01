e24735af2142d2b53de7027e3a2e2272cf043bee
dev Carlos <maiscarlos@gmail.com>
2022-02-01 15:02:44 -0300

    Projeto criado

---
+++ src/main/java/br/com/linecomp/springbootmongodb/WorkshopSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.linecomp.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootMongodbApplication.class, args);
+	}
+
+}
