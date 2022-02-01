5c464b595cb1e728a1f2a36c8ffe7984a642e753
dev Carlos <maiscarlos@gmail.com>
2022-02-01 16:20:01 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/br/com/linecomp/springbootmongodb/resources/UserResource.java
+++ src/main/java/br/com/linecomp/springbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.linecomp.springbootmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,28 +10,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.linecomp.springbootmongodb.domain.User;
+import br.com.linecomp.springbootmongodb.dto.UserDTO;
 import br.com.linecomp.springbootmongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
-	@Autowired //injecao de dependencias para Userservice
+
+	@Autowired // injecao de dependencias para Userservice
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET) // ou @GetMapping
-	public ResponseEntity<List<User>> findAll() { // para montar o body da requisição
-		
-		
-		
+	public ResponseEntity<List<UserDTO>> findAll() { // para montar o body da requisição
+
 //		User maria = new User("1", "Maria Brown", "maria@gmail.com"); /mock
 //		User alex = new User("2", "Alex Green", "alex@gmail.com");
 
-		List<User> list = service.findAll(); //mock new ArrayList<>();
+		List<User> list = service.findAll(); // mock new ArrayList<>();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); // converte cada
+		// objeto da lista para um dto
 
 //		list.addAll(Arrays.asList(maria, alex)); //mock
 
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(listDto);
 
 	}
 
