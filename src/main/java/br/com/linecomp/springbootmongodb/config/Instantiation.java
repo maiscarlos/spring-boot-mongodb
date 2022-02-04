f48e49384b6ee6da539ed086a41c6f6258151f47
dev Carlos <maiscarlos@gmail.com>
2022-02-04 15:19:59 -0300

    Projeção com dados do autor com DTO

--- src/main/java/br/com/linecomp/springbootmongodb/config/Instantiation.java
+++ src/main/java/br/com/linecomp/springbootmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import br.com.linecomp.springbootmongodb.domain.Post;
 import br.com.linecomp.springbootmongodb.domain.User;
+import br.com.linecomp.springbootmongodb.dto.AuthorDTO;
 import br.com.linecomp.springbootmongodb.repository.PostRepository;
 import br.com.linecomp.springbootmongodb.repository.UserRepository;
 
@@ -38,12 +39,14 @@ public class Instantiation implements CommandLineRunner { // necessário para ex
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob)); // salvar todos no mongoDB
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-		userRepository.saveAll(Arrays.asList(maria, alex, bob)); // salvar todos no mongoDB
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
