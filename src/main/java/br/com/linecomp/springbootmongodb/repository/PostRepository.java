ac7f0c226ab55a3566f38419e0ff0774f8a46b01
dev Carlos <maiscarlos@gmail.com>
2022-02-04 16:29:30 -0300

    Consulta simples com @Query

--- src/main/java/br/com/linecomp/springbootmongodb/repository/PostRepository.java
+++ src/main/java/br/com/linecomp/springbootmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package br.com.linecomp.springbootmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.linecomp.springbootmongodb.domain.Post;
@@ -10,7 +11,10 @@ import br.com.linecomp.springbootmongodb.domain.Post;
 //acesso a bancos
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
--- src/main/java/br/com/linecomp/springbootmongodb/services/PostService.java
+++ src/main/java/br/com/linecomp/springbootmongodb/services/PostService.java
@@ -25,7 +25,8 @@ public class PostService {
 	}
 
 	public List<Post> findByTitle(String text) {
-		return repo.findByTitleContainingIgnoreCase(text);
+//		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
 
 }
